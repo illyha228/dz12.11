f7e63f607817c1ff6317910785d9934ae521ca2e
Korshun Illya <korsillua@gmail.com>
2024-11-12 15:29:43 +0200

    ex2

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-with open('text.txt', 'r') as origin_file:
-    origin_data = [line.strip() for line in origin_file.readlines()]
+from unicodedata import digit
 
-with open('out.txt', 'r') as overwrite_file:
-    overwrite_data = [line.strip() for line in overwrite_file.readlines()]
+with open('text.txt', 'r') as f:
+    origin = f.read()
 
-origin_diff = []
+_char = 0
+for char in origin:
+    _char += 1
 
-for line in origin_data:
-    if line not in overwrite_data:
-        origin_diff.append(line)
+_line = 0
+for line in origin.splitlines():
+    _line += 1
 
-print("нем'text.txt',  у 'out.txt':")
-for line in origin_diff:
-    print(line)
+vowel_letters = 0
+consonant_letters = 0
+for i in origin:
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+        vowel_letters += 1
+    else:
+        consonant_letters += 1
+
+digit = 0
+for i in origin:
+    if i.isdigit():
+        digit += 1
+
+
+print(f"Number of characters: {_char}")
+print(f"Number of lines: {_line}")
+print(f"Number of vowel letters: {vowel_letters}")
+print(f"Number of consonant letters: {consonant_letters}")
+print(f"Number of digits: {digit}")
