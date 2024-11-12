010e63913cbdf3dfe6e9a198acb665f074f38ef4
Korshun Illya <korsillua@gmail.com>
2024-11-12 15:57:32 +0200

    lab10

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-НЕ знаю як зробити
+with open('text.txt', 'w') as file:
+    file.write("Message")
+
+with open('text2.txt', 'r') as file:
+    content = file.read()
+
+read = content.split()
+count_words = 0
+
+for i in read:
+    count_words += 1
+
+print(count_words)
