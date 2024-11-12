ccd07c72b64199d5a956130a7f074c61e011f76b
Korshun Illya <korsillua@gmail.com>
2024-11-12 15:53:13 +0200

    lab7

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-a = input("Введіть: ")
-
-with open('text.txt', 'r') as file:
-    content = file.read()
-
-words = content.split()
-count = 0
-
-for word in words:
-    if word.startswith(a):
-        count += 1
-
-print(count)
+НЕ знаю як зробити
