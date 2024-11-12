c3684d04d4d15b0747fa0fdaadb34ed7eddd005b
Korshun Illya <korsillua@gmail.com>
2024-11-12 15:30:47 +0200

    ex3

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-from unicodedata import digit
+with open('text.txt', 'r') as file:
+    content = file.readlines()
 
-with open('text.txt', 'r') as f:
-    origin = f.read()
+reversed_content = content[::-1]
 
-_char = 0
-for char in origin:
-    _char += 1
-
-_line = 0
-for line in origin.splitlines():
-    _line += 1
-
-vowel_letters = 0
-consonant_letters = 0
-for i in origin:
-    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
-        vowel_letters += 1
-    else:
-        consonant_letters += 1
-
-digit = 0
-for i in origin:
-    if i.isdigit():
-        digit += 1
-
-
-print(f"Number of characters: {_char}")
-print(f"Number of lines: {_line}")
-print(f"Number of vowel letters: {vowel_letters}")
-print(f"Number of consonant letters: {consonant_letters}")
-print(f"Number of digits: {digit}")
+with open('overwrite.txt', 'w') as output_file:
+    output_file.writelines(reversed_content)
