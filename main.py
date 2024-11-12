9c9cc0adbeecf1665968020d2d3c0992d57dc782
Korshun Illya <korsillua@gmail.com>
2024-11-12 15:46:09 +0200

    lab2

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 with open('text.txt') as file:
     content = file.read()
 
-new_text = []
-word_count = 0
-
-for word in content.split():
-    if len(word) > 6:
-        word_count += 1
-        new_text.append(f"{word_count}: {word}")
-
 with open('out.txt', 'w') as output_file:
-    output_file.write("\n".join(new_text))
+    output_file.writelines(content)
